447adf5b60818d125bae418d3f22a2458958ff48
Chris Henning <saturos@gmail.com>
2013-01-22 21:30:33 -0500

    Bump version number for p10

--- sage-frontend.cc
+++ sage-frontend.cc
@@ -33,7 +33,7 @@ const char *usage =
     "  --                    End of options to Sage; further options are for ADOM";
 
 const char *version =
-    "ADOM Sage version 0.9.5a - frontend for ADOM\n"
+    "ADOM Sage version 0.9.7 - frontend for ADOM\n"
     "ADOM is (C) Copyright 1994-2012 Thomas Biskup.\n"
     "ADOM Sage is Copyright (c) 2002 Joshua Kelley.\n"
     "ADOM Sage is Copyright (c) 2012 Christopher Henning.\n";
